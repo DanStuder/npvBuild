b3cfac54afd76b89bcb0cff6408af4898d0fb582
DanStuder <100282876+DanStuder@users.noreply.github.com>
2022-10-06 11:53:05 +0200

    improved tick-marks

--- R/z_plot.R
+++ R/z_plot.R
@@ -7,9 +7,10 @@
 #' @param xmax Definiert Breite des Plots. 4 sieht meistens gut aus.
 #' @param xlab Move the annotation on the x axis
 #' @param ylab Move the annotation on the y axis
-#' @importFrom ggplot2 ggplot aes geom_vline labs theme_minimal theme annotate element_text stat_function
+#' @importFrom ggplot2 ggplot aes geom_vline labs theme_minimal theme annotate element_text stat_function scale_x_continuous element_blank
 #' @importFrom stats dnorm
 #' @importFrom dplyr if_else
+#' @importFrom scales number_format
 #' @export
 
 z.plot = function(z, tail = c("less", "greater", "two.sided"), xmax = 4, xlab = (xmax/10), ylab = 0.001) {
@@ -40,12 +41,17 @@ z.plot = function(z, tail = c("less", "greater", "two.sided"), xmax = 4, xlab = 
              x = z + xlab,
              y = 0.2 + ylab,
              label = paste0('z = ', round(z,4))) +
+    scale_x_continuous(breaks = c(seq(-xmax, 2.5, by = 0.5),
+                                  seq(-1.5, 1.5, by = 0.5),
+                                  seq(2.5, xmax, by = 0.5)),
+                       labels = number_format(accuracy = 0.1)) +
 
     # set axis labels
     labs(x = "z",
          y = "p") +
     theme_minimal() +
-    theme(axis.title.y = element_text(angle = 0, vjust = 0.5))
+    theme(axis.title.y = element_text(angle = 0, vjust = 0.5),
+          panel.grid.minor = element_blank())
 
 
   # now the red part, that marks the significance, is added based on the value of z.crit
